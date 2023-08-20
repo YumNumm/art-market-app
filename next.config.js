bc6d2938498f51c66107c7728e541f39f65a8128
Restyled.io <commits@restyled.io>
2023-08-20 20:53:56 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
+  reactStrictMode : true,
+  trailingSlash : true,
+  images : {
+    unoptimized : true,
   },
-  output: "export",
-  redirects: {
+  output : "export",
+  redirects : {
     async redirects() {
-      return [
-        {
-          source: "/art-market/:id",
-          destination: "/art-market/"
-        }
-      ];
+      return [ {source : "/art-market/:id", destination : "/art-market/"} ];
     }
   }
 };
