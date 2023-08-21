990a81ee4da43db296991f9e113aee6c53cf95c6
YumNumm <onory0917@gmail.com>
2023-08-21 15:59:34 +0900

    fix: mono font

--- src/app/art-market/page.tsx
+++ src/app/art-market/page.tsx
@@ -38,7 +38,7 @@ class ApiError extends Error {
 const notFound = (
   <>
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">404 Page Not Found</h1>
+      <h1 className="text-4xl font-bold font-mono">404 Page Not Found</h1>
     </div>
   </>
 );
